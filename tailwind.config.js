037f2430b036a9ec3ecf64ce52ebe315dff0b2a5
sohagmia360 <gdsohag360@gmail.com>
2022-02-16 13:01:46 +0600

    hinish header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        primary_color : "#202124",
+        secondary_color : "#525355",
+      },
+    
+    },
   },
   plugins: [],
 }
